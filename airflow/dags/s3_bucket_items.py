3a9f026f0efc2d4d8190066e573488f4b9475132
command7 <vijay_saravanan@outlook.com>
2019-08-07 17:31:12 -0400

    Feat: Create a DAG that connects to an S3 bucket daily and logs the name of items present.

---
+++ airflow/dags/s3_bucket_items.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+import logging, datetime
+
+
+def log_bucket_items():
+    s3_hook = S3Hook(aws_conn_id='aws_credentials')
+    bucket_name = Variable.get('s3_bucket')
+    bucket_prefix = Variable.get('s3_prefix')
+    bucket_items = s3_hook.list_keys(bucket_name=bucket_name, prefix=bucket_prefix)
+    for bucket_item in bucket_items:
+        logging.info(f"- s3://{bucket_name}/{bucket_prefix}/{bucket_item}")
+
+
+bucket_keys_dag = DAG(
+    'S3_bucket_items',
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+    schedule_interval='@daily'
+)
+
+
+list_bucket_items_task = PythonOperator(
+    task_id='Log_bucket_keys.task',
+    python_callable=log_bucket_items,
+    dag=bucket_keys_dag
+)
