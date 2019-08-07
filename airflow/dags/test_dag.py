d451ddb14bca632fa970d07648956485030802fb
command7 <vijay_saravanan@outlook.com>
2019-08-06 23:24:32 -0400

    Misc: Update gitignore file

---
+++ airflow/dags/test_dag.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import datetime, logging
+
+def say_hello():
+    logging.info("Hello the dag works")
+
+
+dag = DAG(
+    'first_task.task',
+    start_date=datetime.datetime.now()
+)
+
+operator_task = PythonOperator(
+    task_id='first_running_task',
+    dag=dag,
+    python_callable=say_hello
+)
