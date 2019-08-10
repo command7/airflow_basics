4e76caaed308a360998d8d14d88970942757e9d1
command7 <vijay_saravanan@outlook.com>
2019-08-09 20:08:22 -0400

    Feat: Validate if all files were copied successfully after copying from one s3 bucket to another.

--- airflow/dags/s3_to_s3_copy.py
+++ airflow/dags/s3_to_s3_copy.py
@@ -23,23 +23,35 @@ def copy_contents_to_local():
             logging.info(f'{object_key} copied as {destination_filename}')
 
 
-# def validate_s3_t0_s3_copy():
-#     source_filenames = list()
-#     s3_hook = S3Hook(aws_conn_id='aws_credentials')
-#     udacity_bucket = Variable.get('udacity_s3_bucket')
-#     udacity_bucket_prefix = Variable.get('udacity_s3_prefix')
-#     my_bucket = Variable.get('my_s3_bucket')
-#
-#     udacity_object_keys = s3_hook.list_keys(bucket_name=udacity_bucket,
-#                                             prefix=udacity_bucket_prefix)
-#     # my_bucket_keys = s3_hook.list_keys(bucket_name=my_bucket)
-#
-#     for source_object_key in udacity_object_keys:
-#         if source_object_key.endswith('csv'):
-#             filename = source_object_key.split('/')[-1]
-#             test = s3_hook.check_for_wildcard_key(wildcard_key=filename,
-#                                                   bucket_name=my_bucket)
-#             logging.info(f'Testing: {test}')
+def validate_s3_t0_s3_copy():
+    missing_files = list()
+    s3_hook = S3Hook(aws_conn_id='aws_credentials')
+    udacity_bucket = Variable.get('udacity_s3_bucket')
+    udacity_bucket_prefix = Variable.get('udacity_s3_prefix')
+    my_bucket = Variable.get('my_s3_bucket')
+
+    udacity_object_keys = s3_hook.list_keys(bucket_name=udacity_bucket,
+                                            prefix=udacity_bucket_prefix)
+
+    for source_object_key in udacity_object_keys:
+        if source_object_key.endswith('csv'):
+            key_to_check = source_object_key[15:]
+            logging.info(f'Checking if {key_to_check} exists.')
+            test = s3_hook.check_for_key(key=key_to_check,
+                                         bucket_name=my_bucket)
+            if test:
+                logging.info(f'File: {key_to_check} has been successfully copied.')
+            else:
+                missing_files.append(source_object_key)
+                logging.info(f'File: {key_to_check} was not copied.')
+
+    if len(missing_files) == 0:
+        logging.info("All files copied.")
+    else:
+        logging.info("The following files were not copied.")
+        for missing_file in missing_files:
+            logging.info(missing_file)
+
 
 copy_dag = DAG(
     'Copy_data_between_s3buckets',
@@ -53,8 +65,10 @@ copy_task = PythonOperator(
     dag=copy_dag
 )
 
-# validate_task = PythonOperator(
-#     task_id='Check_copied_files.task',
-#     python_callable=validate_s3_t0_s3_copy,
-#     dag=copy_dag
-# )
+validate_task = PythonOperator(
+    task_id='Check_copied_files.task',
+    python_callable=validate_s3_t0_s3_copy,
+    dag=copy_dag
+)
+
+copy_task >> validate_task
